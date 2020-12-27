6421ca9d85c8eacbc53892f249935862e5a8ab6f
dalgleishandrew <76648213+dalgleishandrew@users.noreply.github.com>
2020-12-27 13:14:50 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,12 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    CommandSpeed = receivedNumber
+})
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "Forward") {
-        pins.servoWritePin(AnalogPin.P2, LeftForward)
-        pins.servoWritePin(AnalogPin.P1, RightForward)
+        pins.servoWritePin(AnalogPin.P2, CommandSpeed)
+        pins.servoWritePin(AnalogPin.P1, 180 - CommandSpeed)
+        basic.showNumber(CommandSpeed)
         BrightLights.showColor(neopixel.colors(NeoPixelColors.White))
-        basic.showLeds(`
-            . . # . .
-            . . . # .
-            # # # # #
-            . . . # .
-            . . # . .
-            `)
     }
     if (receivedString == "Spin") {
         pins.servoWritePin(AnalogPin.P2, LeftSpin)
@@ -24,41 +21,28 @@ radio.onReceivedString(function (receivedString) {
             `)
     }
     if (receivedString == "Stop") {
-        pins.servoWritePin(AnalogPin.P2, LeftStop)
+        pins.servoWritePin(AnalogPin.P2, RightStop)
+        pins.servoWritePin(AnalogPin.P1, LeftStop)
         BrightLights.clear()
         BrightLights.show()
-        pins.servoWritePin(AnalogPin.P1, RightStop)
         basic.clearScreen()
     }
     if (receivedString == "Back") {
-        pins.servoWritePin(AnalogPin.P2, LeftBack)
-        pins.servoWritePin(AnalogPin.P1, RightBack)
+        pins.servoWritePin(AnalogPin.P2, 180 - CommandSpeed)
+        pins.servoWritePin(AnalogPin.P1, CommandSpeed)
+        basic.showNumber(CommandSpeed)
         BrightLights.showColor(neopixel.colors(NeoPixelColors.Red))
-        basic.showLeds(`
-            . . # . .
-            . # . . .
-            # # # # #
-            . # . . .
-            . . # . .
-            `)
     }
 })
+let CommandSpeed = 0
 let RightStop = 0
 let LeftStop = 0
 let RightSpin = 0
 let LeftSpin = 0
-let RightBack = 0
-let LeftBack = 0
-let RightForward = 0
-let LeftForward = 0
 let BrightLights: neopixel.Strip = null
 radio.setGroup(1)
 BrightLights = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
 BrightLights.setBrightness(200)
-LeftForward = 180
-RightForward = 0
-LeftBack = 0
-RightBack = 180
 LeftSpin = 180
 RightSpin = 180
 LeftStop = 90
